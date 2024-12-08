ce22d3e4b5f9ef04ddca48403f3d3b008d8bdcf2
Elizaveta-Kuzmina <kuzzmina19@gmail.com>
2024-12-08 12:49:49 +0700

    Create admin_add_user.php

---
+++ lab10/admin_add_user.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+include 'db.php';
+
+
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
+    header("Location: login.php");
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
+
+   
+    $stmt = $pdo->prepare("INSERT INTO users (username, email, password, role) VALUES (:username, :email, :password, :role)");
+    $stmt->execute([
+        'username' => $username,
+        'email' => $email,
+        'password' => $password,
+        'role' => $role
+    ]);
+
+    header("Location: admin.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Добавить пользователя</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Добавить пользователя</h1>
+    </header>
+    <div class="container">
+        <form action="admin_add_user.php" method="post">
+            <label for="username">Имя пользователя:</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Пароль:</label>
+            <input type="password" id="password" name="password" required>
+
+            <label for="role">Роль:</label>
+            <select id="role" name="role" required>
+                <option value="user">Пользователь</option>
+                <option value="admin">Администратор</option>
+            </select>
+<br>
+            <button type="submit">Добавить</button>
+        </form>
+        <a href="admin.php">Назад</a>
+    </div>
+  
+</body>
+</html>
