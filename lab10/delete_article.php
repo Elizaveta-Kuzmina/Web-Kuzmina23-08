5d5441154aaf7d5d22e8f221a58e7abb75862f5f
Elizaveta-Kuzmina <kuzzmina19@gmail.com>
2024-12-08 12:52:43 +0700

    Create delete_article.php

---
+++ lab10/delete_article.php
@@ -0,0 +1,28 @@
+<?php
+include 'db.php';
+session_start();
+
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['article_id'])) {
+    $articleId = (int)$_POST['article_id'];
+    $userId = $_SESSION['user_id'];
+
+  
+    $stmt = $pdo->prepare("DELETE FROM articles WHERE id = :id AND author_id = :author_id");
+    $stmt->execute([
+        'id' => $articleId,
+        'author_id' => $userId
+    ]);
+
+    header("Location: personal_articles.php?success=Статья удалена.");
+    exit;
+} else {
+    header("Location: personal_articles.php?error=Некорректный запрос.");
+    exit;
+}
